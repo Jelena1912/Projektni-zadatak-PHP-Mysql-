05e0c0ee0d4736231c7125de0945a05f5821724e
Jelena <81640389+Jelena1912@users.noreply.github.com>
2021-11-27 11:52:18 +0100

    Add files via upload

--- projektni zadatak 4/signin.php
+++ projektni zadatak 4/signin.php
@@ -24,29 +24,32 @@ include ("dbconn.php");
 	}
 	else if ($_POST['_action_'] == TRUE) {
 
-		$query  = "SELECT * FROM users";
-		$query .= " WHERE username='" .  $_POST['username'] . "'";
-		$result = @mysqli_query($conn, $query);
-		$row = @mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $query = "SELECT * FROM users";
+        $query .= " WHERE username='" . $_POST['username'] . "'";
+        $result = @mysqli_query($conn, $query);
+        $row = @mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-		if (password_verify($_POST['password'], $row['password'])) {
-			#password_verify https://secure.php.net/manual/en/function.password-verify.php
-			$_SESSION['user']['valid'] = 'true';
-			$_SESSION['user']['id'] = $row['id'];
-			$_SESSION['user']['firstname'] = $row['firstname'];
-			$_SESSION['user']['lastname'] = $row['lastname'];
-			$_SESSION['message'] = '<p>Dobrodošli, ' . $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['lastname'] . '</p>';
-			# Redirect to admin website
-			header("Location: index.php?menu=8");
-		}
-
-		# Bad username or password
-		else {
-			unset($_SESSION['user']);
-			$_SESSION['message'] = '<p>Upisali ste krivu e-mail adresu!</p>';
-			header("Location: index.php?menu=7");
-		}
-	}
+        if ($row['valid'] == 0) {
+            unset($_SESSION['user']);
+            echo '<p>Korisnik mora biti odobren od administratora! <a href="index.php?menu=7">Ponovi</a> </p>';
+        } else {
+            if (password_verify($_POST['password'], $row['password'])) {
+                #password_verify https://secure.php.net/manual/en/function.password-verify.php
+                $_SESSION['user']['valid'] = 'true';
+                $_SESSION['user']['id'] = $row['id'];
+                $_SESSION['user']['firstname'] = $row['firstname'];
+                $_SESSION['user']['lastname'] = $row['lastname'];
+                $_SESSION['message'] = '<p>Dobrodošli, ' . $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['lastname'] . '</p>';
+                # Redirect to admin website
+                header("Location: index.php?menu=8");
+            } # Bad username or password
+            else {
+                unset($_SESSION['user']);
+                $_SESSION['message'] = '<p>Upisali ste krivu e-mail adresu!</p>';
+                header("Location: index.php?menu=7");
+            }
+        }
+    }
 	print '
 	</div>';
 ?>
